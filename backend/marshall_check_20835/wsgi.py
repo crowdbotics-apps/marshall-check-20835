821cd3bd354f8176f066c4435b23027a926e671d
Crowdbotics <team@crowdbotics.com>
2020-09-29 18:37:05 +0000

    Update models

--- backend/marshall_check_20835/wsgi.py
+++ backend/marshall_check_20835/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'marshall_check_20835.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "marshall_check_20835.settings")
 
 application = get_wsgi_application()
